ad2b3512669a369a0ed483dc2f28b178b721ea9a
payn873 <142923202+payn873@users.noreply.github.com>
2024-12-04 17:55:40 -0600

    Update BiTree.h
    Added proper destructor and some test functions that will be deleted later, and updated the parse, parenIndex, and isValid functions to handle strings instead of char arrays.

--- Code/BiTree.h
+++ Code/BiTree.h
@@ -1,16 +1,19 @@
 #include "BiTrNode.h"
+#include <string>
+using namespace std;
 
 class BiTree
 {
    public:
       BiTree();
-      void testInsert();
-      void testPrint();
-      void parenIndex(char arr[], int size, int* priIndex);
-      bool isValid(char arr[], int size);
-      void parse(char arr[], BiTrNode* curNode);
+      ~BiTree();
+      void test();
+      void testPrint(BiTrNode* curNode);
+      void parenIndex(const string& str, int size, int* priIndex);
+      bool isValid(const string& str, int size);
       void Clear();
    private:
       BiTrNode* root;
       void deleteNodes(BiTrNode* node);
+      void parse(const string& str, BiTrNode* curNode);
 };
