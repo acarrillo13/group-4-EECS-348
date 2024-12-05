4f5ab440c2ac7e83cfb42ac8935861696bea889f
payn873 <142923202+payn873@users.noreply.github.com>
2024-12-04 21:01:44 -0600

    Update BiTree.h
    Added calculate and print functions

--- Code/BiTree.h
+++ Code/BiTree.h
@@ -7,13 +7,14 @@ class BiTree
    public:
       BiTree();
       ~BiTree();
-      void test();
-      void testPrint(BiTrNode* curNode);
       void parenIndex(const string& str, int size, int* priIndex);
       bool isValid(const string& str, int size);
       void Clear();
+      string calculateEq(const string& str);
    private:
       BiTrNode* root;
       void deleteNodes(BiTrNode* node);
       void parse(const string& str, BiTrNode* curNode);
+      void recCalculateEq(const string& str, BiTrNode* curNode);
+      void Print(BiTrNode* curNode);
 };
