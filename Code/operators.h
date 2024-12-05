a4857768d59b24447091759d206dfcefd3b1c52a
payn873 <142923202+payn873@users.noreply.github.com>
2024-12-04 18:46:01 -0600

    Add files via upload

---
+++ Code/operators.h
@@ -0,0 +1,14 @@
+#include <cmath>
+using namespace std;
+
+double add(double n1, double n2);
+
+double subtract(double n1, double n2);
+
+double multiply(double n1, double n2);
+
+double divide(double n1, double n2);
+
+int modulate(int n1, int n2);
+
+double exponent(double n1, double  n2);
