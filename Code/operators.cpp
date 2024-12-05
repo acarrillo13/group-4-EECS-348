5cd1b7cab5d7a703b3b9a12ec55c86d3acf0738c
payn873 <142923202+payn873@users.noreply.github.com>
2024-12-05 15:06:29 -0600

    Update operators.cpp
    Added error handling

--- Code/operators.cpp
+++ Code/operators.cpp
@@ -4,57 +4,107 @@
 using namespace std;
 
 string add(const string& n1, const string& n2){
-    double dn1 = extractNumeric(n1);
-    double dn2 = extractNumeric(n2);
-    double sum = dn1 + dn2;
-    return to_string(sum);
+    if(n1 == "e" || n2 == "e"){
+        string err = "e";
+        return err;
+    }else{
+        double dn1 = extractNumeric(n1);
+        double dn2 = extractNumeric(n2);
+        double sum = dn1 + dn2;
+        return to_string(sum);
+    }
 }
 
 string subtract(const string& n1, const string& n2){
-    double dn1 = extractNumeric(n1);
-    double dn2 = extractNumeric(n2);
-    double subed = dn1 - dn2;
-    return to_string(subed);
+    if(n1 == "e" || n2 == "e"){
+        string err = "e";
+        return err;
+    }else{
+        double dn1 = extractNumeric(n1);
+        double dn2 = extractNumeric(n2);
+        double subed = dn1 - dn2;
+        return to_string(subed);
+    }
 }
 
 string multiply(const string& n1, const string& n2){
-    double dn1 = extractNumeric(n1);
-    double dn2 = extractNumeric(n2);
-    double mult = dn1 * dn2;
-    return to_string(mult);
+    if(n1 == "e" || n2 == "e"){
+        string err = "e";
+        return err;
+    }else{    
+        double dn1 = extractNumeric(n1);
+        double dn2 = extractNumeric(n2);
+        double mult = dn1 * dn2;
+        return to_string(mult);
+    }
 }
 
 string divide(const string& n1, const string& n2){
-    double dn1 = extractNumeric(n1);
-    double dn2 = extractNumeric(n2);
-    double div = dn1 / dn2;
-    return to_string(div);
+    if(n1 == "e" || n2 == "e"){
+        string err = "e";
+        return err;
+    }else{    
+        double dn1 = extractNumeric(n1);
+        double dn2 = extractNumeric(n2);
+        if(dn2 == 0){
+            string err = "e";
+            return err;
+        }else{
+            double div = dn1 / dn2;
+            return to_string(div);
+        }
+    }  
 }
 
 string modulate(const string& n1, const string& n2){
-    double dn1 = extractNumeric(n1);
-    double dn2 = extractNumeric(n2);
-    int in1 = static_cast<int>(dn1);
-    int in2 = static_cast<int>(dn2);
-    int modu = in1%in2;
-    return to_string(modu);
+    if(n1 == "e" || n2 == "e"){
+        string err = "e";
+        return err;
+    }else{
+        double dn1 = extractNumeric(n1);
+        double dn2 = extractNumeric(n2);
+        int in1 = static_cast<int>(dn1);
+        int in2 = static_cast<int>(dn2);
+        if(in2 == 0){
+            string err = "e";
+            return err;
+        }else{
+            int modu = in1 / in2;
+            return to_string(modu);
+        }
+    }
 }
 
 string exponent(const string& n1, const string& n2){
-    double dn1 = extractNumeric(n1);
-    double dn2 = extractNumeric(n2);
-    double exp = pow(dn1,dn2);
-    return to_string(exp);
+    if(n1 == "e" || n2 == "e"){
+        string err = "e";
+        return err;
+    }else{
+        double dn1 = extractNumeric(n1);
+        double dn2 = extractNumeric(n2);
+        double exp = pow(dn1,dn2);
+        return to_string(exp);
+    }
 }
 
 string positive(const string& n1){
-    double dn1 = extractNumeric(n1);
-    double adn1 = abs(dn1);
-    return to_string(adn1);
+    if(n1 == "e"){
+        string err = "e";
+        return err;
+    }else{
+        double dn1 = extractNumeric(n1);
+        double adn1 = abs(dn1);
+        return to_string(adn1);
+    }
 }
 
 string negative(const string& n1){
-    double dn1 = extractNumeric(n1);
-    double ndn1 = abs(dn1) * -1;
-    return to_string(ndn1);
+    if(n1 == "e"){
+        string err = "e";
+        return err;
+    }else{    
+        double dn1 = extractNumeric(n1);
+        double ndn1 = abs(dn1) * -1;
+        return to_string(ndn1);
+    }
 }
