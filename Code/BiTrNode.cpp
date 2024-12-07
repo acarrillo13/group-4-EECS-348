86775a93d6f956b1b5129949fd5016ab45ed0ea3
payn873 <142923202+payn873@users.noreply.github.com>
2024-12-06 18:07:42 -0600

    Update BiTrNode.cpp
    Added comments

--- Code/BiTrNode.cpp
+++ Code/BiTrNode.cpp
@@ -1,5 +1,8 @@
 #include "BiTrNode.h"
 #include <string>
+/*
+Creates a binary node to be used in a binary tree
+*/
 using namespace std;
 BiTrNode::BiTrNode(const string& data, BiTrNode* left, BiTrNode* right) 
      : value(data),left(left),right(right){}
